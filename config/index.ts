45bdaedc0b93bfc675c10e65feade9e795c07c73
Juan Gabriel Borges Santos <130366568+JuanGBS@users.noreply.github.com>
2024-11-16 09:06:36 -0300

    Add files via upload

---
+++ config/index.ts
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Frontier",
+  description:
+    "Plataforma interativa para aprendizagem de idiomas com aulas, questionários e acompanhamento de progresso.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "duolingo-clone",
+    "learn-language",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "lingo",
+    "postgresql",
+    "sonner",
+    "drizzle",
+    "zustand",
+    "mysql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>
+}
